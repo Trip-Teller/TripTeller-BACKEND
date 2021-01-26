e7e3ef80c36f3fd961841cad14b01322326f4c63
uju_sy <lsyeon1997@ajou.ac.kr>
2021-01-26 16:51:19 +0900

    #34 eslint-plugin-mocha 설치 및 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,25 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
+    mocha: true,
     node: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:mocha/recommended',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
     ecmaVersion: 12,
   },
+  plugins: [
+    'mocha',
+  ],
   rules: {
+    'prefer-arrow-callback': 'off',
+    'mocha/prefer-arrow-callback': 'error',
   },
 };
