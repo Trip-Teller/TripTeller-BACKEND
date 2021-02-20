aa26368ceb5f8249c97ed7025ce61fab1d705730
uju_sy <lsyeon1997@ajou.ac.kr>
2021-02-21 03:26:36 +0900

    #49 컨설턴트 생성 API 구현

--- utils/storageUpload.js
+++ utils/storageUpload.js
@@ -5,18 +5,29 @@ const { Storage } = require('@google-cloud/storage');
 if (env !== 'ci') {
   if (!process.env.STORAGE_USERCONTENTS_HOST) throw new Error('STORAGE_USERCONTENTS 환경변수가 설정되어 있지 않습니다.');
 }
-const bucketName = 'usercontents';
+const userContentsBucketName = 'usercontents';
 
 const storage = new Storage();
 
 const storageUploadUserProfileImage = (fileData) => new Promise((resolve, reject) => {
-  storage.bucket(bucketName).upload(`${fileData.path}`, {
+  storage.bucket(userContentsBucketName).upload(`${fileData.path}`, {
     destination: `/profile/${fileData.filename}`,
   }, (err, data) => {
     if (err) reject(err);
     else resolve(data);
   });
 });
+
+const storageUploadBackgroundImage = (fileData) => new Promise((resolve, reject) => {
+  storage.bucket(userContentsBucketName).upload(`${fileData.path}`, {
+    destination: `/background/${fileData.filename}`,
+  }, (err, data) => {
+    if (err) reject(err);
+    else resolve(data);
+  });
+});
+
 module.exports = {
   storageUploadUserProfileImage,
+  storageUploadBackgroundImage,
 };
