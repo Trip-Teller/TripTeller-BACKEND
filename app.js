df8fdc12348088ab1a463eedc87cd08bb71edda6
uju_sy <lsyeon1997@ajou.ac.kr>
2021-03-29 23:21:39 +0900

    #73 컨설팅 신청 API 구현
    #73 컨설팅 신청 API 구현

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 
 const authRouter = require('./routes/auth');
 const consultantRouter = require('./routes/consultatnt');
+const consultingDataRouter = require('./routes/consulting');
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/consultant/v0', consultantRouter.v0.router);
+app.use('/consultingData/v0', consultingDataRouter.v0.router);
 app.use('/auth/v0', authRouter.v0.router);
 app.use('/user/v0', userRouter.v0.router);
 
